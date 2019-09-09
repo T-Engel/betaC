d6d87fd7a054c51de07928f72aa8525bd81676e1
Thore Engel <thore.engel@idiv.de>
2019-09-09 15:32:01 +0200

    Add code for rarefaction curves

---
+++ R/rarefaction_curves.R
@@ -0,0 +1,42 @@
+#' Calculate IBR curves at alpha and gamma scales and return them in a long format
+#'
+#' Please load tidyverse before using this.
+#'
+#' @param x a site by species abundance matrix
+#'
+#' @return a data.frame
+#'
+
+rarefy_long <- function(x) {
+    if(is.matrix(x)==F) x=matrix(x,nrow = 1, byrow =T, dimnames= list("x", names(x)))
+    alphas <-
+        lapply(row.names(x), function(i)
+            return(as.numeric(vegan::rarefy(
+                x[i, ], sample = 1:sum(x[i, ])
+            )))) %>%
+        lapply(function(x)
+            return(data.frame(
+                S_n = as.numeric(x), N = 1:length(x)
+            )))
+    names(alphas) <- rownames(x)
+    alphas <- alphas %>% plyr::ldply(.id = "Curve")
+    alphas$type = "minor"
+    mean_alpha <-
+        data.frame(
+            Curve = "mean_alpha",
+            S_n = colMeans(as.matrix(vegan::rarefy(
+                x, 1:min(rowSums(x))
+            ))),
+            N = 1:min(rowSums(x)),
+            type = "major"
+        )
+    gamma <-
+        data.frame(
+            Curve = "gamma",
+            S_n = as.numeric(vegan::rarefy(colSums(x), 1:sum(x))),
+            N = 1:sum(x),
+            type = "major"
+        )
+    out = alphas %>% full_join(mean_alpha, by = c("Curve", "S_n", "N",  "type")) %>% full_join(gamma, by = c("Curve", "S_n", "N", "type"))
+
+}
